2c38cdec4083e5dfb000f60ae1d17752f599834d
Guewen Baconnier <guewen.baconnier@camptocamp.com>
2014-06-25 08:59:20 +0200

    Push branches to GitHub

--- tools/copy_branches.py
+++ tools/copy_branches.py
@@ -1,12 +1,4 @@
 # -*- coding: utf-8 -*-
-from __future__ import absolute_import, print_function
-
-import argparse
-import os
-import subprocess
-from contextlib import contextmanager
-from pkg_resources import resource_string
-import yaml
 
 """
 
@@ -16,25 +8,30 @@ Dependency:
 
 """
 
+from __future__ import absolute_import, print_function
+
+import argparse
+import os
+import re
+import subprocess
+from contextlib import contextmanager
+from pkg_resources import resource_string
+import yaml
+
+
+@contextmanager
+def cd(path):
+    cwd = os.getcwd()
+    os.chdir(path)
+    yield
+    os.chdir(cwd)
 
 
 class Migrate(object):
 
-    def __init__(self, path):
+    def __init__(self, path, dry_run=False):
         self.path = path
-        self.level = 0
-
-    @contextmanager
-    def cd(self, path):
-        cwd = os.getcwd()
-        os.chdir(path)
-        self.level += 1
-        yield
-        os.chdir(cwd)
-        self.level -= 1
-
-    def print(self, *args):
-        print('  ' * self.level, *args)
+        self.dry_run = dry_run
 
     def _clone_bzr(self, bzr_branch):
         # we keep the serie's name so we can handle both projects:
@@ -42,41 +39,58 @@ class Migrate(object):
         # lp:banking-addons/bank-statement-reconcile-7.0
         name = bzr_branch.replace('lp:', '').replace('/', '-')
         repo = os.path.join(self.path, name)
-        self.print('Working on', repo)
+        print('Working on', repo)
         if os.path.exists(repo):
-            with self.cd(repo):
-                self.print('git fetch', 'from', bzr_branch)
+            with cd(repo):
+                print('  git fetch', 'from', bzr_branch)
                 subprocess.check_output(['git', 'fetch'])
         else:
-            with self.cd(self.path):
-                self.print('git clone', repo, 'from', bzr_branch)
+            with cd(self.path):
+                print('  git clone', repo, 'from', bzr_branch)
                 subprocess.check_output(['git', 'clone',
                                          "bzr::%s" % bzr_branch,
                                          repo])
         return repo
 
-    def _add_bzr_branch(self, repo, bzr_branch, gh_branch):
-        with self.cd(repo):
-            remotes = subprocess.check_output(['git', 'remote', '-v'])
+    def _add_remote(self, repo, name, remote):
+        with cd(repo):
+            remotes = subprocess.check_output(['git', 'remote'])
             remotes = remotes.split('\n')
-            remotes = set(remote.split('\t')[0] for remote in remotes)
-            if gh_branch not in remotes:
-                self.print('git remote add', gh_branch, bzr_branch)
+            if name not in remotes:
+                print('  git remote add', name, remote)
                 subprocess.check_output(['git', 'remote', 'add',
-                                        gh_branch,
-                                        "bzr::%s" % bzr_branch])
-            self.print('git fetch', gh_branch, 'from', bzr_branch)
+                                        name, remote])
+
+    def _add_bzr_branch(self, repo, bzr_branch, gh_branch):
+        with cd(repo):
+            self._add_remote(repo, gh_branch, "bzr::%s" % bzr_branch)
+            print('  git fetch', gh_branch, 'from', bzr_branch)
             subprocess.check_output(['git', 'fetch', gh_branch])
 
-    def _push_to_github(self, repo, gh_url):
-        with self.cd(repo):
-            self.print('git push', gh_url)
+    def _push_to_github(self, repo, refs):
+        with cd(repo):
+            print('  git push github', refs)
+            if not self.dry_run:
+                subprocess.check_output(
+                    ['git', 'push', 'github', refs])
 
-    def copy_branches(self):
+    def _push_tags_to_github(self, repo):
+        with cd(repo):
+            print('  git push github --tags')
+            if not self.dry_run:
+                subprocess.check_output(
+                    ['git', 'push', 'github', '--tags'])
+
+    def copy_branches(self, only_projects=None):
         projects = resource_string(__name__, 'branches.yaml')
         projects = yaml.load(projects)
         for project in projects['projects']:
             gh_url = project['github']
+            if only_projects:
+                gh_name = re.search('git@github.com:OCA/(.+).git',
+                                    gh_url).group(1)
+                if gh_name not in only_projects:
+                    continue
             master = None
             branches = []
             for source, target in project['branches']:
@@ -88,18 +102,29 @@ class Migrate(object):
             repo = self._clone_bzr(master)
             for source, target in branches:
                 self._add_bzr_branch(repo, source, target)
-            self._push_to_github(repo, gh_url)
+            self._add_remote(repo, 'github', gh_url)
+            self._push_to_github(repo, 'origin/master')
+            self._push_tags_to_github(repo)
+            for __, gh_branch in branches:
+                refs = ('refs/remotes/{branch}/master:'
+                        'refs/heads/{branch}'.format(branch=gh_branch))
+                self._push_to_github(repo, refs)
 
 
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("path",
                         help="Branches directory")
+    parser.add_argument("--dry-run", dest="dry_run", action='store_true')
+    parser.add_argument("--projects", nargs='*',
+                        help="Name of the Github projects that you want to "
+                             "migrate.")
+    parser.set_defaults(dry_run=False)
     args = parser.parse_args()
     if not os.path.exists(args.path):
         exit("Path %s does not exist" % args.path)
-    migration = Migrate(os.path.abspath(args.path))
-    migration.copy_branches()
+    migration = Migrate(os.path.abspath(args.path), dry_run=args.dry_run)
+    migration.copy_branches(only_projects=args.projects)
 
 
 if __name__ == '__main__':
