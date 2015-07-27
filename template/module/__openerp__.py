09b119dd66a61f5303c7753baf84107c4c0245c3
Jairo Llopis <j.llopis@grupoesoc.es>
2015-07-27 13:23:29 +0200

    Add template magic files. Required in all modules.

---
+++ template/module/__openerp__.py
@@ -0,0 +1,15 @@
+# -*- encoding: utf-8 -*-
+{
+    "name": "Module name",
+    "summary": "Module summary",
+    "version": "0.1.0",
+    "author": "Odoo Community Association (OCA)",
+    "category": "Module category",
+    "depends": [
+        "base",
+    ],
+    "demo": [
+    ],
+    "data": [
+    ],
+}
