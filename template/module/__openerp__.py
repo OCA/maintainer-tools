d6015252f892f2bf88c2a5d463c614b841b8a779
Jairo Llopis <j.llopis@grupoesoc.es>
2015-09-07 12:23:25 +0200

    Copyright first.

--- template/module/__openerp__.py
+++ template/module/__openerp__.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
+# © <YEAR(S)> <AUTHOR(S)>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
-# © <YEAR(s)> <AUTHOR(s)>
 {
     "name": "Module name",
     "summary": "Module summary",
