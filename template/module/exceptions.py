1faee89935f1851e71875acdcaee779d458d35ae
Jairo Llopis <j.llopis@grupoesoc.es>
2015-11-05 13:22:49 +0100

    Add module-level exceptions file.

---
+++ template/module/exceptions.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+# © <YEAR(S)> <AUTHOR(S)>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+from openerp import _, exceptions
+
+
+class ModuleNameValidationError(exceptions.ValidationError):
+    """Base class for this module's validation errors."""
+    def __init__(self, *args, **kwargs):
+        self._args, self._kwargs = args, kwargs
+        value = self._message()
+        super(ModuleNameValidationError, self).__init__(value)
+
+    @staticmethod
+    def _message(self):
+        """Format the message."""
+        return self.__doc__.format(*self._args, **self._kwargs)
+
+
+class WrongNameError(ModuleNameValidationError):
+    """Name {} is wrong."""
+
+
+class TranslatedWrongNameError(ModuleNameValidationError):
+    __doc__ = _("Name {} is wrong.")
