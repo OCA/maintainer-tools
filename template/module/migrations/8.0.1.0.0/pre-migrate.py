fdb25bb42fde00b371a1a827cfbaf3cc13dacfaa
Jairo Llopis <j.llopis@grupoesoc.es>
2015-11-16 13:41:41 +0100

    Add migrations to template.

---
+++ template/module/migrations/8.0.1.0.0/pre-migrate.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+# © <YEAR(S)> <AUTHOR(S)>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+
+def migrate(cr, version):
+    """Update database from previous versions, before updating module."""
+    pass
