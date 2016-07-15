ca0941c654a4c319686aff9cd82b66ab0fb9a06b
Yajo <Yajo@users.noreply.github.com>
2016-07-15 23:03:33 +0200

    [FIX] `use strict` inside functions. (#205)
    Odoo automatically minifies and concatenates all scripts. Given [`use strict` may only be used at the beginning of a script or of a function](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Strict_mode), minified version would not be strict unless it is declared inside the function. Even worse, a strict script loaded as the 1st one could affect other non-strict ones loaded later.
    
    So, in short, this should be considered a guideline.

--- template/module/static/src/js/module_name.js
+++ template/module/static/src/js/module_name.js
@@ -1,7 +1,7 @@
 /* Copyright <YEAR(S)> <AUTHOR(S)>
  * License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl). */
 
-"use strict";
 (function ($) {
+    "use strict";
     // Script that will be loaded when document is ready
 })(jQuery);
