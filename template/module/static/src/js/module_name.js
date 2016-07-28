1b81178754788b41b02244c3583c64eb9cf087e2
Dave Lasley <dave@laslabs.com>
2016-07-28 11:00:53 -0700

    [MIG] template: Upgrade JS API * Upgrade JS to new API * Upgrade onload method to use core bus web_client_ready * Add an exported method * Create website module and namespace web modules

--- template/module/static/src/js/module_name.js
+++
@@ -1,7 +0,0 @@
-/* Copyright <YEAR(S)> <AUTHOR(S)>
- * License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl). */
-
-(function ($) {
-    "use strict";
-    // Script that will be loaded when document is ready
-})(jQuery);
