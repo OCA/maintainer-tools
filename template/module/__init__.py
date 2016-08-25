7cf503c0e85de75be71dad5f3fd321c4c0c702d1
Pedro M. Baeza <pedro.baeza@gmail.com>
2016-08-25 11:22:13 +0200

    Unfold imports

--- template/module/__init__.py
+++ template/module/__init__.py
@@ -2,5 +2,8 @@
 # Copyright <YEAR(S)> <AUTHOR(S)>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from . import models, wizards, controllers, report
+from . import models
+from . import wizards
+from . import controllers
+from . import report
 from .hooks import pre_init_hook, post_load, post_init_hook, uninstall_hook
