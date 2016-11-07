3f474e22d43ab222f98f244be4e3a7d6f9c99da8
Dave Lasley <dave@laslabs.com>
2016-11-07 08:28:14 -0800

    [ADD] template: PhantomJS Test Sample (#215)

---
+++ template/module/static/tests/js/module_name.js
@@ -0,0 +1,19 @@
+/* Copyright <YEAR(S)> <AUTHOR(S)>
+ * License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl). */
+
+odoo.define_section('module_name', ['module_name.ExportedObject'], function(test) {
+    "use strict";
+
+    test('It should demonstrate a PhantomJS test for web (backend)',
+        function(assert, ExportedObject) {
+            var expect = 'Expected Return',
+                result = new ExportedObject();
+            assert.assertStrictEqual(
+                result,
+                expect,
+                "Result !== Expect and the test failed with this message"
+            );
+        }
+    );
+    
+});
