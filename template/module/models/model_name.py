cd877c2c40f504892f71da4b660cda22cd47cfc3
Dave Lasley <dave@laslabs.com>
2017-09-13 11:16:55 -0700

    [IMP] GNU links should be https

--- template/module/models/model_name.py
+++ template/module/models/model_name.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 # Copyright <YEAR(S)> <AUTHOR(S)>
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 
 from openerp import models
 
--- tests/test_repo/module1/__init__.py
+++ tests/test_repo/module1/__init__.py
@@ -1,3 +1,3 @@
 # -*- coding: utf-8 -*-
 # © <YEAR(S)> <AUTHOR(S)>
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
