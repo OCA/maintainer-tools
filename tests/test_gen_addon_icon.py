4d3777f76b4477d6ecdf8a7a59af42fb0513f125
mreficent <miquel.raich@eficent.com>
2019-03-21 19:32:15 +0100

    [ADD] tools: icon generator

---
+++ tests/test_gen_addon_icon.py
@@ -0,0 +1,26 @@
+# License AGPLv3 (http://www.gnu.org/licenses/agpl-3.0-standalone.html)
+# Copyright (c) 2019 Eficent Business and IT Consulting Services S.L.
+#        (http://www.eficent.com)
+
+import os
+import subprocess
+import sys
+
+from tools.gen_addon_icon import ICONS_DIR, ICON_TYPE
+
+
+def test_gen_addon_icon(tmp_path):
+    addon_dir = tmp_path / "addon"
+    addon_dir.mkdir()
+    with (addon_dir / "__manifest__.py").open("w") as f:
+        f.write(u"{'name': 'addon'}")
+    cmd = [
+        sys.executable,
+        "-m",
+        "tools.gen_addon_icon",
+        "--addon-dir",
+        str(addon_dir),
+    ]
+    subprocess.check_output(cmd, stderr=subprocess.STDOUT)
+    assert os.path.exists(
+        os.path.join(addon_dir._str, ICONS_DIR, 'icon.%s' % ICON_TYPE))
