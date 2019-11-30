bcbe593e365dcf72f4ddadb24f551c62fa099745
Bhavesh Odedra <bodedra@ursainfosystems.com>
2019-11-30 17:36:49 +0530

    [IMP] Change tag,name,weblink from openerp to odoo

--- template/module/models/abstract_something.py
+++ template/module/models/abstract_something.py
@@ -2,7 +2,7 @@
 # Copyright <YEAR(S)> <AUTHOR(S)>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from openerp import models
+from odoo import models
 
 
 class AbstractSomething(models.AbstractModel):
--- template/module/models/model_name.py
+++ template/module/models/model_name.py
@@ -2,7 +2,7 @@
 # Copyright <YEAR(S)> <AUTHOR(S)>
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 
-from openerp import models
+from odoo import models
 
 
 class ModelName(models.Model):
