d7ee640b130693fbc74e689c85137a663922903f
Pedro M. Baeza <pedro.baeza@tecnativa.com>
2020-01-31 18:35:26 +0100

    [ADD] Massive changer:
    This script helps to make a massive change across a whole organization
    on contents of a file.
    
    Installation
    ============
    
    For using this utility, you need to install these dependencies:
    
    * github3.py library for handling Github calls. To install it, use:
      ``sudo pip install github3.py``.
    
    Configuration
    =============
    
    You must have a file called oca.cfg on the same folder of the script for
    storing credentials parameters. You can generate an skeleton config running
    this script for a first time.
    
    Usage
    =====
    oca-massive-change [-h] [-p PROJECTS [PROJECTS ...]] [-e EMAIL]
                       [-t TARGET_ORG] [-m COMMIT_MESSAGE]
                       target path source_string target_string
    
    Performs a massive change over a whole organization.
    
    positional arguments:
      target                Target branch
      path                  File path
      source_string         Source string (or regexp)
      target_string         Target string (or regexp)
    
    optional arguments:
      -h, --help            show this help message and exit
      -p PROJECTS [PROJECTS ...], --projects PROJECTS [PROJECTS ...]
                            List of specific projects to migrate
      -e EMAIL, --email EMAIL
                            Provides an email address used to commit on GitHub if
                            the one associated to the GitHub account is not public
      -t TARGET_ORG, --target-org TARGET_ORG
                            By default, the GitHub organization used is OCA. This
                            arg lets you provide an alternative organization
      -m COMMIT_MESSAGE, --message COMMIT_MESSAGE
                            By default, '[UPD] Massive change [skip ci]', but can
                            be provided with an alternative one
    
    This script will perform the following operations for each project:
    
    * Look for the selected file in the target branch.
    * If found, modify it searching for the source string and replacing it by the
      target string.
    * Commit the changes with optional commit message.

--- setup.py
+++ setup.py
@@ -62,6 +62,7 @@ setuptools.setup(
             'oca-sync-users = tools.oca_sync_users:main',
             'oca-autopep8 = tools.autopep8_extended:main',
             'oca-gen-addons-table = tools.gen_addons_table:gen_addons_table',
+            'oca-massive-change = tools.massive_change:main',
             'oca-migrate-branch = tools.migrate_branch:main',
             'oca-migrate-branch-empty = tools.migrate_branch_empty:main',
             'oca-pypi-upload = tools.pypi_upload:cli',
