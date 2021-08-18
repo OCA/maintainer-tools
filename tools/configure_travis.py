237d94584f3df3ecfd6292319e3374039b17179e
Stéphane Bidoul <stephane.bidoul@acsone.eu>
2021-08-18 16:57:40 +0200

    Update configure_travis to travis-ci.com api

--- tools/configure_travis.py
+++ tools/configure_travis.py
@@ -1,30 +1,23 @@
 # License AGPLv3 (http://www.gnu.org/licenses/agpl-3.0-standalone.html)
-from __future__ import print_function
-import time
-
 import click
 import requests
 
-from .oca_projects import get_repositories
 
-
-OCA_TRAVIS_GITHUB_USER = 'oca-travis'
-OCA_TRAVIS_GITHUB_EMAIL = 'oca+oca-travis@odoo-community.org'
+OCA_TRAVIS_GITHUB_USER = "oca-travis"
+OCA_TRAVIS_GITHUB_EMAIL = "oca+oca-travis@odoo-community.org"
 
 
 class Travis(object):
-
     def __init__(self, travis_token):
-        self.api_url = 'https://api.travis-ci.org'
+        self.api_url = "https://api.travis-ci.com"
         self.headers = {
-            'Accept': 'application/vnd.travis-ci.2.1+json',
-            'Authorization': 'token ' + travis_token,
+            "Travis-API-Version": "3",
+            "Authorization": "token " + travis_token,
         }
         self.count = 0
 
     def request(self, method, url, json=None):
         self.count += 1
-        print(self.count, method, url, json)
         r = requests.request(
             method,
             self.api_url + url,
@@ -37,65 +30,69 @@ class Travis(object):
         return r.json()
 
     def get(self, url):
-        return self.request('get', url)
+        return self.request("GET", url)
 
     def post(self, url, json):
-        return self.request('post', url, json=json)
+        return self.request("POST", url, json=json)
 
     def delete(self, url):
-        return self.request('delete', url)
+        return self.request("DELETE", url)
 
-    def set_env_var(self, repo_id, name, value):
-        env_vars = self.get(
-            '/settings/env_vars?repository_id={}'.format(repo_id),
-        )
-        found = None
-        for env_var in env_vars['env_vars']:
-            if env_var['name'] == name:
-                found = env_var
-                break
-        if found:
-            if value and found['value'] == value:
-                # variable already set
-                return
-            # variable exists with another value (or is private so we
-            # don't know its value), delete it
-            self.delete(
-                '/settings/env_vars/{}?repository_id={}'.
-                format(found['id'], repo_id),
-            )
-        # add variable
-        self.post(
-            '/settings/env_vars?repository_id={}'.format(repo_id),
-            json={
-                'env_var': {
-                    'name': name,
-                    'value': value,
-                },
-            },
-        )
+    def patch(self, url, json):
+        return self.request("PATCH", url, json=json)
+
+    def set_env_vars(self, org, repo, vars):
+        repo_slug = f"{org}%2F{repo}"
+        existing_vars = self.get(f"/repo/github/{repo_slug}/env_vars")
+        existing_vars_by_name = {
+            v["name"]: v for v in existing_vars["env_vars"]
+        }
+        for var_name, var_value in vars.items():
+            json = {
+                "env_var.name": var_name,
+                "env_var.value": var_value,
+                "env_var.public": False,
+            }
+            if var_name in existing_vars_by_name:
+                print(f"Updating {var_name} in {repo}")
+                var_id = existing_vars_by_name[var_name]["id"]
+                self.patch(f"/repo/github/{repo_slug}/env_var/{var_id}", json)
+            else:
+                print(f"Creating {var_name} in {repo}")
+                self.post(f"/repo/github/{repo_slug}/env_vars", json)
 
 
 @click.command()
-@click.option('--travis-token', required=True, envvar='TRAVIS_TOKEN',
-              help="Find your travis token at "
-                   "https://travis-ci.org/profile")
-@click.option('--repo', help="repo name (default=all)")
-@click.option('--oca-travis-github-token', required=True,
-              prompt="oca-travis github token (from OCA Board keepass file)",
-              help="Find this in the OCA Board keepass.")
+@click.option(
+    "--travis-token",
+    required=True,
+    envvar="TRAVIS_TOKEN",
+    help="Find your travis token at " "https://travis-ci.com/account/preferences",
+)
+@click.option("--repo", help="repo name (default=all)")
+@click.option(
+    "--oca-travis-github-token",
+    required=True,
+    prompt="oca-travis github token (from OCA Board keepass file)",
+    help="Find this in the OCA Board keepass.",
+)
 def main(repo, oca_travis_github_token, travis_token):
-    """ Configure Travis for OCA on one or all projects. """
+    """Configure Travis for OCA on one or all projects."""
     if repo:
         repos = [repo]
     else:
+        from .oca_projects import get_repositories
+
         repos = get_repositories()
     travis = Travis(travis_token)
     for repo in repos:
         print("Configuring travis for", repo)
-        repo_id = travis.get('/repos/OCA/' + repo)['repo']['id']
-        travis.set_env_var(repo_id, 'GITHUB_USER', OCA_TRAVIS_GITHUB_USER)
-        travis.set_env_var(repo_id, 'GITHUB_EMAIL', OCA_TRAVIS_GITHUB_EMAIL)
-        travis.set_env_var(repo_id, 'GITHUB_TOKEN', oca_travis_github_token)
-        # rate limit
-        time.sleep(20)
+        travis.set_env_vars(
+            "OCA",
+            repo,
+            {
+                "GITHUB_USER": OCA_TRAVIS_GITHUB_USER,
+                "GITHUB_EMAIL": OCA_TRAVIS_GITHUB_EMAIL,
+                "GITHUB_TOKEN": oca_travis_github_token,
+            },
+        )
